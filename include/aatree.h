898dce0f2260f905822bd138415e837f0f714906
toadkarter <vrakhmanin1@gmail.com>
2024-08-20 17:48:54 +0100

    Adds basic insert function to AA Tree.

--- include/aatree.h
+++ include/aatree.h
@@ -1,4 +1,19 @@
 #ifndef AATREE_H_
 #define AATREE_H_
 
+#include <bits/stdint-uintn.h>
+#include <stdbool.h>
+
+typedef struct Node
+{
+  uint8_t key;
+
+  uint8_t level;
+  struct Node *left;
+  struct Node *right;
+} Node;
+
+void init();
+void insert(uint8_t key, Node *tree, bool *success);
+
 #endif
--- src/aatree.c
+++ src/aatree.c
@@ -1,1 +1,60 @@
 #include "aatree.h"
+#include <malloc.h>
+#include <stddef.h>
+
+inline static void skew(Node* node)
+{
+    Node *temp;
+
+    if (node->left != NULL && node->left->level == node->level)
+    {
+        temp = node;
+        node = node->left;
+        temp->left = node->right;
+        node->right = temp;
+    }
+}
+
+inline static void split(Node* node)
+{
+    Node *temp;
+
+    if (node->right != NULL && node->right->right != NULL && node->right->right->level == node->level)
+    {
+        temp = node;
+        node = node->right;
+        temp->right = node->left;
+        node->left = temp;
+        node->level = node->level + 1;
+    }
+}
+
+void insert(uint8_t key, Node *tree, bool *success)
+{
+    if (tree == NULL)
+    {
+        tree = malloc(sizeof(Node));
+        tree->left = NULL;
+        tree->right = NULL;
+        tree->level = 1;
+        *success = true;
+    }
+    else
+    {
+        if (key < tree->key)
+        {
+            insert(key, tree->left, success);
+        }
+        else if (key > tree->key)
+        {
+            insert(key, tree->right, success);
+        }
+        else
+        {
+            *success = false;
+        }
+
+        skew(tree);
+        split(tree);
+    }
+}
